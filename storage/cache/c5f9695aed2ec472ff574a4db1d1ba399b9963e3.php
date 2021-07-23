f664ca9d2784109f14be008aa92e81af001c2b8a
Ahmed Mohamed Ibrahim <eng.ahmedmohamed.2002@gmail.com>
2021-07-24 01:58:04 +0200

    update

--- storage/cache/c5f9695aed2ec472ff574a4db1d1ba399b9963e3.php
+++
@@ -1,8 +0,0 @@
-<link rel="stylesheet" href="<?php echo e(asset('styles/style.css')); ?>">
-<link rel="icon" href="public/pics/DinoPHP-icon.png">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Tajawal:wght@200;300;400;500;700;800;900&display=swap" rel="stylesheet">
-<?php /**PATH C:\xampp\htdocs\DinoPHP-web\local\views/links.blade.php ENDPATH**/ ?>
