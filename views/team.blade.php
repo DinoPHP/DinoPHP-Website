dbf5e3999978107b0d5ab4c61127c3afa337a5c5
Ahmed Mohamed Ibrahim <eng.ahmedmohamed.2002@gmail.com>
2021-07-24 19:34:26 +0200

    Update team.blade.php

--- views/team.blade.php
+++ views/team.blade.php
@@ -15,15 +15,19 @@
 
     <div class="row">
         <div class="col-lg-3">
-            <img src="{{ asset('pics/team/ahmedmohamed.jpg') }}" style="width: 280px;height: 280px;object-fit: cover;border-radius: .5rem!important;">
-            <div class="info" style="margin-top: 7%;">
-                <h4 class="name" style="font-weight: bold;color: #e92644;">Ahmed Mohamed</h4>
-                <h5 class="address" style="color: #585858;font-weight: 400;">Alexandria, Egypt</h5>
-                <div class="social" style="display: flex;flex-direction: row;gap: 25px;">
-                    <a href="https://twitter.com/ahmedmohammedx"><i class="fab fa-twitter" style="font-size: 22px;color: #3e3e3e;"></i></a>
-                    <a href="https://github.com/Ahmed-Ibrahimm"><i class="fab fa-github" style="font-size: 22px;color: #3e3e3e;"></i></a>
+
+            <div class="person" style="margin-top: 5%;">
+                <img src="{{ asset('pics/team/ahmedmohamed.jpg') }}" style="width: 280px;height: 280px;object-fit: cover;border-radius: .5rem!important;">
+                <div class="info" style="margin-top: 7%;">
+                    <h4 class="name" style="font-weight: bold;color: #e92644;">Ahmed Mohamed</h4>
+                    <h5 class="address" style="color: #585858;font-weight: 400;">Alexandria, Egypt</h5>
+                    <div class="social" style="display: flex;flex-direction: row;gap: 25px;">
+                        <a href="https://twitter.com/ahmedmohammedx"><i class="fab fa-twitter" style="font-size: 22px;color: #3e3e3e;"></i></a>
+                        <a href="https://github.com/Ahmed-Ibrahimm"><i class="fab fa-github" style="font-size: 22px;color: #3e3e3e;"></i></a>
+                    </div>
                 </div>
             </div>
+
         </div>
     </div>
 </div>
