8ebbd04df220d5e63e2b99100911512090194eec
Ahmed Mohamed Ibrahim <eng.ahmedmohamed.2002@gmail.com>
2021-07-24 08:00:27 +0200

    Update team.blade.php

---
+++ views/team.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Our Team - DinoPHP Framework</title>
+@include('links')
+</head>
+
+<body>
+@include('navbar')
+
+</body>
+@include('scripts')
+</html>
