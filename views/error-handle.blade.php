8dbc94ac980830ed7fce58d24ca3c4f6a2488a5b
Ahmed Mohamed Ibrahim <eng.ahmedmohamed.2002@gmail.com>
2021-07-24 20:11:22 +0200

    Update error-handle.blade.php

--- views/error-handle.blade.php
+++ views/error-handle.blade.php
@@ -38,144 +38,66 @@
 
                 <a href="#basics"><h4 style="font-weight: bold"><span class="hashtag">#</span> How To Write Exception</h4></a>
                 <p style="font-size: 1rem;line-height: 1.8rem;color:#2b2e38">
-                    When you start a new DinoPHP project, error and exception handling is already configured for you. The src\Exceptions\Whoops class is where all exceptions thrown by your application are logged and then rendered to the user. We'll dive deeper into this class throughout this documentation.
+                    This is an example of how to write a function for handle the errors for know where is this error in your code and more details like requests information and etc ..
                 </p>
 
-                <p style="font-size: 1rem;line-height: 1.8rem;color:#2b2e38">
-                    Blade views may be returned from routes or controller using the global view helper. Of course, as mentioned in the documentation on views, data may be passed to the Blade view using the view helper's second argument:
-                </p>
                 <div class="code">
 					<?php
-					highlight_string("
+					highlight_string('
 <?php
-
-Route::get('/', function () {
-    return view('greeting', ['name' => 'Finn']);
-});
-
+if (1 != 1) {
+	throw new \Exception("This is error");
+}
 ?>
-                        ");
+                        ');
 					?>
                 </div>
 
+                <p style="font-size: 1rem;line-height: 1.8rem;color:#2b2e38">
+                    This is an error handling for invalid callback function to show us what the error in this callback
+                </p>
 
-                <div id="root-directory">
-
-                    <a href=""><h5 style="font-weight: bold"><span class="hashtag">#</span> Displaying Data</h5></a>
-                    <p style="font-size: 1rem;line-height: 1.8rem;color:#2b2e38">
-                        You may display data that is passed to your Blade views by wrapping the variable in curly braces. For example, given the following route:
-                    </p>
-
-                    <div class="code">
-						<?php
-						highlight_string("
+                <div class="code">
+					<?php
+					highlight_string('
 <?php
-Route::get('/', function () {
-    return view('welcome', ['name' => 'Samantha']);
-});
-?>
-                        ");
-						?>
-                    </div>
-
-                    <p style="font-size: 1rem;line-height: 1.8rem;color:#2b2e38">
-                        You may display the contents of the name variable like so:
-                    </p>
-
-                    <div class="code">
-						<?php
-						highlight_string('
-<?php
-Hello, {{ $name }}.
+if (is_callable($callback)) {
+	call_user_func($callback);
+} else {
+	throw new \BadFunctionCallException("Please provide valid callback function");
+}
 ?>
                         ');
-						?>
-                    </div>
+					?>
+                </div>
 
-                    <a href=""><h5 style="font-weight: bold"><span class="hashtag">#</span> Rendering JSON</h5></a>
-                    <p style="font-size: 1rem;line-height: 1.8rem;color:#2b2e38">
-                        Sometimes you may pass an array to your view with the intention of rendering it as JSON in order to initialize a JavaScript variable. For example:
-                    </p>
-                    <div class="code">
-						<?php
-						highlight_string('
-<?php echo json_encode($array); ?>
-                        ');
-						?>
-                    </div>
+                <p style="font-size: 1rem;line-height: 1.8rem;color:#2b2e38">
+                    This is an error handling for Reflection to show us what the error :
+                </p>
 
-                    <p style="font-size: 1rem;line-height: 1.8rem;color:#2b2e38">
-                        However, instead of manually calling json_encode, you may use the @ json Blade directive. The @ json directive accepts the same arguments as PHP's json_encode function. By default, the @ json directive calls the json_encode function with the JSON_HEX_TAG, JSON_HEX_APOS, JSON_HEX_AMP, and JSON_HEX_QUOT flags:
-                    </p>
-                    <div class="code">
-						<?php
-						highlight_string('
+                <div class="code">
+					<?php
+					highlight_string('
 <?php
-    $app = @json($array);
+throw new \ReflectionException("This is Reflection error");
 ?>
                         ');
-						?>
-                    </div>
+					?>
+                </div>
 
-                    <a href=""><h5 style="font-weight: bold"><span class="hashtag">#</span> Displaying Unescaped Data</h5></a>
-                    <p style="font-size: 1rem;line-height: 1.8rem;color:#2b2e38">
-                        By default, Blade {{}} statements are automatically sent through PHP's htmlspecialchars function to prevent XSS attacks. If you do not want your data to be escaped, you may use the following syntax:
-                    </p>
+                <p style="font-size: 1rem;line-height: 1.8rem;color:#2b2e38">
+                    How to write a code for handling invalid argument errors ? This is an example of simple code :
+                </p>
 
-                    <div class="code">
-						<?php
-						highlight_string('
+                <div class="code">
+					<?php
+					highlight_string('
 <?php
-Hello, {!! $name !!}.
+throw new \InvalidArgumentException("This is argument exception");
 ?>
                         ');
-						?>
-                    </div>
-
-                    <a href=""><h5 style="font-weight: bold"><span class="hashtag">#</span> If Statements</h5></a>
-                    <p style="font-size: 1rem;line-height: 1.8rem;color:#2b2e38">
-                        You may construct if statements using the @ if, @ elseif, @ else, and @ endif directives. These directives function identically to their PHP counterparts:
-                    </p>
-
-                    <div class="code">
-						<?php
-						highlight_string('
-<?php
-
-@if (count($records) === 1)
-    I have one record!
-@elseif (count($records) > 1)
-    I have multiple records!
-@else
-    I dont have any records!
-@endif
-
-?>
-                        ');
-						?>
-                    </div>
-
-                    <p style="font-size: 1rem;line-height: 1.8rem;color:#2b2e38">
-                        In addition to the conditional directives already discussed, the @ isset and @ empty directives may be used as convenient shortcuts for their respective PHP functions:
-                    </p>
-
-                    <div class="code">
-						<?php
-						highlight_string('
-<?php
-
-@isset($records)
-    // $records is defined and is not null...
-@endisset
-
-@empty($records)
-    // $records is "empty"...
-@endempty
-
-?>
-                        ');
-						?>
-                    </div>
+					?>
+                </div>
 
                 </div>
             </div>
