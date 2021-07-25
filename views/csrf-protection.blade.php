9c6386d6ce7081093b2623cf54b38641165debe7
Ahmed Mohamed Ibrahim <eng.ahmedmohamed.2002@gmail.com>
2021-07-25 13:46:52 +0200

    Update csrf-protection.blade.php

--- views/csrf-protection.blade.php
+++ views/csrf-protection.blade.php
@@ -48,12 +48,7 @@
                         </p>
 
                         <div class="code">
-                            <?php
-                                highlight_string('
-                                &lt;input type="hidden" name="token" value="{{ create_csrf() }}"&gt;
-                                ')
-                            ?>
-
+&lt;input type="hidden" name="token" value="{{ create_csrf() }}"&gt;
                         </div>
 
                     </div>
