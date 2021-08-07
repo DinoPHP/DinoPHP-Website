47dd82d8a7ef2a63a096ae18382eae6660184711
Ahmed Mohamed Ibrahim <eng.ahmedmohamed.2002@gmail.com>
2021-08-07 02:02:04 +0200

    Update links.php

--- views/links.php
+++ views/links.php
@@ -14,5 +14,5 @@
 <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/11.2.0/styles/default.min.css">
 <script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/11.2.0/highlight.min.js"></script>
 <?php
-header('X-Framework: DinoPHP');
+header('X-Powered-By: DinoPHP');
 ?>
